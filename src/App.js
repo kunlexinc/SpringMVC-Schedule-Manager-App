1bbe1f6304bc188989896f6e4ef3312039e5aba2
kunlexinc <mkunle6@gmail.com>
2022-09-19 12:33:32 +0100

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import Counter from "./components/counter/Counter";
 import "./App.css";
+import TodoApp from "./components/todo/TodoApp";
 
 function App() {
   return (
     <div className="App">
-      <Counter by={1} />
+      {/* <Counter by={1} />
       <Counter by={5} />
-      <Counter by={10} />
+      <Counter by={10} /> */}
+      <TodoApp />
     </div>
   );
 }
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 class TodoApp extends Component {
   render() {
-    return <div className="TodoApp">My todo Application</div>;
+    return (
+      <div className="TodoApp">
+        <LoginComponent></LoginComponent>
+      </div>
+    );
+  }
+}
+
+class LoginComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "irisTech",
+      password: "",
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+  }
+
+  handleUsernameChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={this.handleUsernameChange}
+        />
+        Password:{" "}
+        <input type="password" name="password" value={this.state.password} />
+        <button>Login</button>
+      </div>
+    );
   }
 }
 
